a0b14ded5358c0867348b4b38d24c4f018c4a9c8
dengwenj <927561237@qq.com>
2022-04-13 23:07:19 +0800

    属性问题

--- src/02_函数高级/01_原型与原型链/03_原型链.js
+++ src/02_函数高级/01_原型与原型链/03_原型链.js
@@ -38,3 +38,23 @@ console.log(Function.__proto__ === Function.prototype) // true
 
 // Object 的原型对象是原型链尽头
 console.log(Object.prototype.__proto__) // null 
+
+/**
+ * 读取对象的属性值时：会自动到原型链中查找
+ * 设置对象的属性值时：不会查找原型链，如果当前对象中没有此属性，直接添加此属性并设置其值
+ * 方法一般定义在原型中，属性一般在实例对象自身上
+ * 
+ * 实例对象的隐式原型的值等于构造函数的显式原型的值
+ */
+function Person(name, age) {
+  this.name = name
+  this.age = age
+}
+Person.prototype.say = function () {
+  console.log('say')
+}
+const p1 = new Person('dwj', 22)
+const p2 = new Person('ww', 17)
+p2.say = 'say'
+console.log(p1.say) // 还是那个函数
+console.log(p2) // Person {name: 'ww', age: 17, say: 'say'}
