b0e1c9b00e10e486f0149649053a387dd976a16e
dengwenj <927561237@qq.com>
2022-04-16 23:18:22 +0800

    异常穿透和值传递

--- src/05_promise/10_手写Promise.js
+++ src/05_promise/10_手写Promise.js
@@ -61,6 +61,18 @@ class DWJPromise {
   }
 
   then(onResolved, onRejected) {
+    if (typeof onRejected !== 'function') {
+      // 实现异常穿透，就是加了个默认值
+      onRejected = (reason) => {
+        throw reason
+      }
+    }
+    if (typeof onResolved !== 'function') {
+      // 值传递，第一个可以不写
+      // p.then().then((res) => { console.log(res) }, () => {})
+      onResolved = res => res
+    }
+
     return new DWJPromise((resolve, reject) => {
       if (this.promiseState === FULFILLED) {
         handleResolveReject.call(this, onResolved, resolve, reject)
@@ -83,44 +95,55 @@ class DWJPromise {
       }
     })
   }
+
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
+  }
 }
 
 /**
  * 测试
  */
 const p = new DWJPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('成功1111')
-  }, 1000)
+  // setTimeout(() => {
+  //   resolve('成功1111')
+  // }, 1000)
 
-  // reject('失败')
+  reject('失败')
   // resolve('成功')
   
   // throw '错误'
 })
 console.log(p)
 
-const p1 = p.then((res) => {
-  console.log(res)
-  // return 111111111111
-  return new DWJPromise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('new promise')
-    }, 3000);
-  })
-}, (reason) => {
-  console.log(reason)
-  // return 222
-  return new DWJPromise((resolve, reject) => {
-    // resolve('resolve')
-    reject('reject')
-  })
+// const p1 = p.then((res) => {
+//   console.log(res)
+//   // return 111111111111
+//   return new DWJPromise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('new promise')
+//     }, 3000);
+//   })
+// })
+// console.log(p1)
+
+p.catch((res) => {
+  console.log(res);
 })
-console.log(p1)
 
 // this.callbackRR = [] 写成数组都会调用，不会覆盖
 // p.then((res) => {
 //   console.log(res)
 // }, (reason) => {
 //   console.log(reason)
+// })
+
+
+// const ffff = new Promise((resolve, reject) => {
+//   reject('时报')
+// })
+
+// const pppp = ffff.catch((rs) => {
+//   console.log(rs)
+
 // })
