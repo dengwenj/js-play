aea24ed7d3f61b3abdc8eb7eaa0f532aa4b10b81
dengwenj <927561237@qq.com>
2022-04-16 22:04:29 +0800

    同步修改状态then方法返回

--- src/05_promise/10_手写Promise.js
+++ src/05_promise/10_手写Promise.js
@@ -39,45 +39,88 @@ class DWJPromise {
   }
 
   then(onResolved, onRejected) {
-    if (this.promiseState === FULFILLED) {
-      onResolved(this.promiseResult)
-    }
+    return new DWJPromise((resolve, reject) => {
+      if (this.promiseState === FULFILLED) {
+        const res = onResolved(this.promiseResult)
+        if (res instanceof DWJPromise) {
+          res.then((res) => {
+            resolve(res)
+          }, (reason) => {
+            reject(reason)
+          })
+          // if (res.promiseState === FULFILLED) {
+          //   resolve(res.promiseResult)
+          // } else {
+          //   reject(res.promiseResult)
+          // }
+        } else {
+          resolve(res)
+        }
+      }
 
-    if (this.promiseState === REJECTED) {
-      onRejected(this.promiseResult)
-    }
+      if (this.promiseState === REJECTED) {
+        const res = onRejected(this.promiseResult)
+        if (res instanceof DWJPromise) {
+          res.then((res) => {
+            resolve(res)
+          }, (reason) => {
+            reject(reason)
+          })
+          // if (res.promiseState === FULFILLED) {
+          //   resolve(res.promiseResult)
+          // } else {
+          //   reject(res.promiseResult)
+          // }
+        } else {
+          resolve(res)
+        }
+      }
 
-    if (this.promiseState === PENDING) {
-      // 保存回调函数
-      this.callbackRR.push({
-        onResolved,
-        onRejected
-      })
-    }
+      if (this.promiseState === PENDING) {
+        // 保存回调函数
+        this.callbackRR.push({
+          onResolved,
+          onRejected
+        })
+      }
+    })
   }
 }
 
+/**
+ * 测试
+ */
 const p = new DWJPromise((resolve, reject) => {
   // setTimeout(() => {
   //   resolve('成功1111')
   // }, 1000)
 
-  reject('失败')
-  // resolve('成功')
+  // reject('失败')
+  resolve('成功')
   
   // throw '错误'
 })
 console.log(p)
 
-p.then((res) => {
+const p1 = p.then((res) => {
   console.log(res)
+  throw 'ggg'
+  return new DWJPromise((resolve, reject) => {
+    resolve('new promise')
+  })
 }, (reason) => {
   console.log(reason)
+  // return 222
+  return new DWJPromise((resolve, reject) => {
+    // resolve('resolve')
+    reject('reject')
+  })
 })
+console.log(p1)
 
 // this.callbackRR = [] 写成数组都会调用，不会覆盖
-p.then((res) => {
-  console.log(res)
-}, (reason) => {
-  console.log(reason)
-})
+// p.then((res) => {
+//   console.log(res)
+// }, (reason) => {
+//   console.log(reason)
+// })
